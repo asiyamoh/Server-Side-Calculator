569d438f0359be99d36a3c54c21dfe04a893a385
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-07-21 14:09:08 -0500

    created a client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,17 @@
 $(document).ready(handleReady);
 
+let firstNum;
+let secondNum;
+
 function handleReady() {
-    
+    $('#equal').on('click', handleEqual);
+
 }
+
+
+function handleEqual(){
+    firstNum = $('#firstNum').val();
+    secondNum = $('#secondNum').val();
+}
+console.log("the first num is",firstNum);
+console.log("the first num is",secondNum);
