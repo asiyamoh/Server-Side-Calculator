0af6fa21cea7f36caeff0ac351022d865f062308
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-07-21 20:22:48 -0500

    made changes to the client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,17 +1,28 @@
 $(document).ready(handleReady);
 
-let firstNum;
-let secondNum;
+let firstNum = 0;
+let secondNum = 0;
+let answer = 0;
 
 function handleReady() {
+    $('#addition').on('click', addition);
     $('#equal').on('click', handleEqual);
+    firstNum = Number[$('#firstNum').val()];
+    secondNum =  Number[$('#secondNum').val()];
 
 }
 
 
 function handleEqual(){
-    firstNum = $('#firstNum').val();
-    secondNum = $('#secondNum').val();
+    // firstNum = $('#firstNum').val();
+    // secondNum = $('#secondNum').val();
+    console.log("the first num is",firstNum);
+    console.log("the second num is",secondNum);
+
 }
-console.log("the first num is",firstNum);
-console.log("the first num is",secondNum);
+
+function addition(){
+    answer = firstNum + secondNum;
+    console.log('the answer is:', answer);
+}
+
