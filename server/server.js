2e0300f4d936715106f9700c746784d6fe17ff73
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-08-03 12:28:07 -0500

    made changes to the POST

--- server/server.js
+++ server/server.js
@@ -9,45 +9,45 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
-let bundle =[
-  {
-    firstNum:1,
-    secondNum:2,
-    operation:'+'
-  }
-];
 
-
+let bundle =[]
 // GET & POST Routes go here
 app.get('/calculator', (req, res) => {
     console.log('inside GET of calculator')
-    res.send(bundle);
+    res.send(bundle)
   })
   
-  app.post('/addcalculator',(req,res) => {
-    let addCalculator = req.body;
-    bundle.push(addCalculator);
-    
-    for(let each of bundle){
-      let firstNum = parseInt(each.firstNum);
-      let secondNum = parseInt(each.secondNum);
-      let answer = 0;
-      if(each.operation === '+'){
-        answer = firstNum + secondNum;
-      }
-      if(each.operation === '-'){
-        answer = firstNum - secondNum;
-      }
-      if(each.operation === '*'){
-        answer = firstNum *  secondNum;
-      }
-      if(each.operation === '/'){
-        answer = firstNum / secondNum;
-      }
-      console.log(answer);
+  app.post('/calculator',(req,res) => {
+    const number1 = req.body.number1
+    const number2 = req.body.number2
+    const operation = req.body.operator
+
+    let  results  =  calculator(number1,number2,operation)
+
+    let newBundle  = {
+      number1,
+      number2,
+      operation,
+      results
     }
-    res.sendStatus(201);
+    bundle.push(newBundle);
+    res.sendStatus(200);
   })
+
+  function calculator(number1,number2,operation){
+    switch (operation) {
+      case '+':
+        return number1 + number2
+      case '-':
+        return  number1 - number2
+      case '*':
+        return  number1 * number2
+      case '/':
+        return number1 / number2
+      default:
+        return null
+    }
+  }
 
 
 app.listen(PORT, () => {
